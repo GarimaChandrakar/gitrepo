845a8224a7fa14fa86d75433532206d79605472b
GarimaChandrakar <garima.chandrakar@ssipmt.com>
2017-10-03 14:32:48 +0530

    Hello

---
+++ Count.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Count
+{
+public static void main(String...k)
+{
+Scanner cs=new Scanner(System.in);
+System.out.println("enter the no.");
+int n=cs.nextInt();
+int c;
+c=n%5;
+if(c==0)
+{
+System.out.println("firstfinger");
+}
+if(c==1)
+{
+System.out.println("second finger");
+}
+if(c==2)
+{
+System.out.println("third finger");
+}
+if(c==3)
+{
+System.out.println("fourth finger");
+}
+if(c==4)
+{
+System.out.println("fifth finger");
+}
+}
+}
+
